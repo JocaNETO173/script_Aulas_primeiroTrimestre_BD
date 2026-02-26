7fec70ad1d8edd4a8dee5f85d425211314d466b0
JocaNETO173 <joaquim.custodio.neto@escola.pr.gov.br>
2026-02-26 11:43:11 -0300

    Add files via upload

---
+++ condicionaisOrInAnd-26-02-2026.sql
@@ -0,0 +1,10 @@
+create schema sucos_vendas;
+
+select * from tabela_de_produtos where sabor = 'manga' and not (tamanho = '470 ml');
+
+select * from tabela_de_produtos
+where sabor in ('manga', 'laranja');
+
+select cidade, idade from tabela_de_clientes
+where cidade in ('Rio de Janeiro', 'São Paulo')
+and idade >= 20;
